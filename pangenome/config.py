7950a44e34966139385f3da7bd40aaa8e605d6ce
mwhitesi <mdwhitsi@gmail.com>
2017-09-29 17:25:06 -0600

    Initializing repo

---
+++ pangenome/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""Config Settings for Pangenome AMR analysis
+
+Defines file locations of critical datasets
+
+
+"""
+
+PANSEQ = {
+	'pangenome_file': '/media/poolhouse/workspace/l_amr/data/PATRIC/salmonella/panseq/pan_genome.txt'
+}
+
+PHENOTYPE = {
+	'amr_file': '/home/matt/workspace/l_amr/patric_tools/data/salmonella/amr.csv'
+}
---
+++ pangenome/utils.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+"""Utility functions
+
+Commonly-used functions for loading and manipulating inputs
+
+"""
+
+import pandas as pd
+import scipy as sp
+import numpy as np
+import re
+
+def read_panseq(file):
+    """Load panseq pangenome file into scipy sparse matrix
+
+    Args:
+        file (str): Panseq pan_genome.txt file
+     
+    Returns:
+        scipy sparse matrix with genome (sample) rows and locus (feature) columns
+
+
+    """
+
+    df = pd.read_table(file, dtype={'Genome': str, 'LocusID': str})
+    df = df.dropna(subset=['Contig'])
+    df = df[['LocusID','Genome']]
+    df = df.sort_values(by='Genome')
+
+    df = df.apply(lambda s: s.astype("category"))
+
+    pg = sp.sparse.coo_matrix((np.ones(df.shape[0]),(df.Genome.cat.codes, df.LocusID.cat.codes)),dtype=np.int8)
+
+    return (pg,df.Genome.cat.categories,df.LocusID.cat.categories)
+
+
+def read_amr(file, genomes):
+    """Load amr resistant/susceptible file into sparse matrix
+
+    Args:
+        file (str): PATRIC amr csv file
+     
+    Returns:
+        sparse matrix with genome (sample) rows and antibiotic (test) columns
+
+
+    """
+
+    df = pd.read_table(file, dtype={'genome_id': str})
+    df = df[['genome_id','antibiotic','resistant_phenotype']]
+
+    df['genome_id'] = df['genome_id'].apply(lambda x: re.sub(r'(\d+)\.(\d+)',r'\1_dot_\2',str(x)))
+
+    i=0
+    df['antibiotic'] = df['antibiotic'].astype("category")
+    df['resistant_phenotype'] = df['resistant_phenotype'].apply(lambda x: 1 if x == 'Resistant' else 0)
+    values = np.empty(shape=(len(genomes),len(df['antibiotic'].unique())))
+    values[:] = np.nan 
+    for g in genomes:
+        subset = df[df['genome_id'] == g]
+        values[i, subset.antibiotic.cat.codes] = subset['resistant_phenotype']
+        i=i+1
+
+    return (values, subset.antibiotic.cat.categories)
